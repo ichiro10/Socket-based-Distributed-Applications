5028ccb6d2ec69bb208f85e975f1dff3edc35775
Ali <Ali.Ghammaz@etu.univ-grenoble-alpes.fr>
2023-01-21 15:44:53 +0100

    Basic file server completed

--- src/clientServer/Client.java
+++ src/clientServer/Client.java
@@ -1,10 +1,12 @@
 package clientServer;
 import java.io.DataInputStream;
+
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.*;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Client {
@@ -14,17 +16,43 @@ public class Client {
 	    Socket soc = new Socket("localhost", 9997);
 	    
 	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Veuillez saisir votre ClientName:");
-	    String str = sc.nextLine();
+	    System.out.println("Please enter your ClientName:");
+	    String client_name = sc.nextLine();
 	    
-	    DataOutputStream d = new DataOutputStream(soc.getOutputStream());
-	    d.writeUTF(str);
-	    d.flush();
+	    DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+	    dos.writeUTF(client_name);
+	    dos.flush();
 	    
 	    DataInputStream dis = new DataInputStream(soc.getInputStream());
 		String m = (String)dis.readUTF(); 
 		System.out.println(m);
+
 		
+		File file = new File("/home/ichiro19/Desktop/Clients/"+client_name);
+        if (!file.exists() && file.mkdir()) {
+                System.out.println("Your personal directory is created! it's located in /home/ichiro19/Desktop/Clients/");
+             } 
+        else {
+               System.out.println("You have already a personal directory!it's located in /home/ichiro19/Desktop/Clients/");
+            }
+       
+		
+	    System.out.println("Please enter the name of the file to download:");
+	    String file_name = sc.nextLine();
+	    
+	    
+	    FileOutputStream fos = new FileOutputStream("/home/ichiro19/Desktop/Clients/"+client_name+"/"+file_name);
+	    dos.writeUTF(file_name);
+	    dos.flush();
+	    
+	    byte[] buffer = new byte[4096];
+	    int bytesRead;
+	    while ((bytesRead = dis.read(buffer)) != -1){
+	      fos.write(buffer, 0, bytesRead);
+	    }
+	    
+	    fos.close();
+	    sc.close();
 		soc.close(); 
 	}
 
--- src/clientServer/Server.java
+++ src/clientServer/Server.java
@@ -3,25 +3,53 @@ package clientServer;
 import java.io.DataInputStream;
 
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Server {
 	
 	public static void main(String[] args) throws IOException {
 		
 		try (ServerSocket ss = new ServerSocket(9997, 3)) {
-			while (true) {
+		   while (true) {
 			 Socket soc= ss.accept();
 			 
 			 DataInputStream dis = new DataInputStream(soc.getInputStream());
-			 String str = (String)dis.readUTF();
+			 String client_name = (String)dis.readUTF();
 			
-			 DataOutputStream d = new DataOutputStream(soc.getOutputStream());
-			 d.writeUTF("Hello "+ str + "!" );
-			 d.flush();
-			 d.close();
-			}
+			 DataOutputStream dos = new DataOutputStream(soc.getOutputStream());
+			 dos.writeUTF("Hello "+ client_name + "!" );
+			 dos.flush();
+			 
+			 File clients_directory = new File("/home/ichiro19/Desktop/Clients");
+				if(!(clients_directory.exists() && clients_directory.isDirectory())) {
+					clients_directory.mkdir(); 
+				}
+			 
+			 String file_name = (String)dis.readUTF();
+			 
+	
+	          File sourceFile = new File("Downloads/"+file_name);		
+			  FileInputStream fis = new FileInputStream(sourceFile);
+		
+			  byte[] buffer = new byte[4096];
+			  int bytesRead;
+			  while ((bytesRead = fis.read(buffer)) != -1) {
+			      dos.write(buffer, 0, bytesRead);
+			    }
+			 
+			  fis.close();
+		      dos.close();
+			 
+		     
+			 }
+		   
+		}catch (IOException e) {
+			e.printStackTrace();
 		}
 
 }
